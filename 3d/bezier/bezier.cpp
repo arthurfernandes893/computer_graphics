daec2ec37b03011a39600658b911d97466e81f4c
Arthur Fernandes <arthurfernandes893@gmail.com>
2025-06-10 07:46:22 -0300

    bezier fixed

--- 3d/bezier/bezier.cpp
+++ 3d/bezier/bezier.cpp
@@ -1,77 +1,72 @@
-struct XYZ
-{
-    float x;
-    float y;
-    float z;
-};
-#define GL_SILENCE_DEPRECATION
-
-#ifndef NI
-    #define NI 18
-    #define NJ 6
-    XYZ inp[NI+1][NJ+1];
-    #define RESOLUTIONI 3*NI
-    #define RESOLUTIONJ 3*NJ
-    XYZ outp[RESOLUTIONI][RESOLUTIONJ];
-#endif
-
-#ifdef __APPLE_CC__
-#include <GLUT/glut.h>
-#else
 #include <GL/glut.h>
-#endif
+#include <cmath>
+#include <ctime>
 
-#include "../materiais.hpp"
-#include <iostream>
-double frameNum = 0;
+struct XYZ {
+    float x, y, z;
+};
 
-// Calcula o polinômio de Bernstein (função de mistura de Bézier)
-double BezierBlend(int k,double mu, int n)
-{
-   int nn,kn,nkn;
-   double blend=1;
+#define NI 18
+#define NJ 6
+XYZ inp[NI+1][NJ+1];
+#define RESOLUTIONI 6*NI
+#define RESOLUTIONJ 6*NJ
+XYZ outp[RESOLUTIONI][RESOLUTIONJ];
 
-   nn = n;
-   kn = k;
-   nkn = n - k;
+static GLfloat branco[] = { 1.0, 1.0, 1.0, 1.0 };
+static GLfloat vermelho_amb[] = { 0.8, 0.2, 0.2, 1.0 };
+static GLfloat vermelho_dif[] = { 1.0, 0.4, 0.4, 1.0 };
+static GLfloat verde_amb[] = { 0.2, 0.8, 0.2, 1.0 };
+static GLfloat verde_dif[] = { 0.4, 0.9, 0.4, 1.0 };
+static GLfloat azul_amb[] = { 0.2, 0.2, 0.8, 1.0 };
+static GLfloat azul_dif[] = { 0.4, 0.4, 0.9, 1.0 };
 
-   while (nn >= 1) {
-      blend *= nn;
-      nn--;
-      if (kn > 1) {
-         blend /= (double)kn;
-         kn--;
-      }
-      if (nkn > 1) {
-         blend /= (double)nkn;
-         nkn--;
-      }
-   }
-   if (k > 0)
-      blend *= pow(mu,(double)k);
-   if (n-k > 0)
-      blend *= pow(1-mu,(double)(n-k));
+int frame_num = 0;
 
-   return(blend);
+double BezierBlend(int k, double mu, int n) {
+    int nn, kn, nkn;
+    double blend = 1;
+
+    nn = n;
+    kn = k;
+    nkn = n - k;
+
+    while (nn >= 1) {
+        blend *= nn;
+        nn--;
+        if (kn > 1) {
+            blend /= (double)kn;
+            kn--;
+        }
+        if (nkn > 1) {
+            blend /= (double)nkn;
+            nkn--;
+        }
+    }
+
+    if (k > 0)
+        blend *= pow(mu, (double)k);
+    if (n - k > 0)
+        blend *= pow(1 - mu, (double)(n - k));
+
+    return blend;
 }
-
-
 
 void generateControlPoint()
 {
 	   int nMax = 2;
 	   int nMin = -2;
 	   int i,j;
-	   srandom(time(NULL));
+	   srand(time(NULL));
 	   for (i=0;i<=NI;i++) {
 	      for (j=0;j<=NJ;j++) {
-	         inp[i][j].x = i;
-	         inp[i][j].y = j;
-	         inp[i][j].z = random()%((nMax+1)-nMin) + nMin;
-	         inp[i][j].z = 4*sin(i * frameNum*0.5);
-	      }
+	            inp[i][j].x = i;
+	            inp[i][j].y = j;
+               inp[i][j].z = 4 * sin(i + frame_num * 0.5);
+            }
 	   }
 }
+
 
 void Surface()
 {
@@ -98,159 +93,106 @@ void Surface()
    }
 
 }
-void drawSurface() {
-   for (int i = 0; i < RESOLUTIONI - 1; i++) {
-      for (int j = 0; j < RESOLUTIONJ - 1; j++) {
-         // Assign colors based on the position of the surface
-         if (i < RESOLUTIONI / 3) {
-            glColor3f(0.0, 0.0, 1.0); // Blue for the first third
-         } else if (i < 2 * RESOLUTIONI / 3) {
-            glColor3f(0.0, 1.0, 0.0); // Green for the second third
-         } else {
-            glColor3f(1.0, 0.0, 0.0); // Red for the last third
-         }
 
-         glBegin(GL_QUADS);
-         glVertex3f(outp[i][j].x, outp[i][j].y, outp[i][j].z);
-         glVertex3f(outp[i + 1][j].x, outp[i + 1][j].y, outp[i + 1][j].z);
-         glVertex3f(outp[i + 1][j + 1].x, outp[i + 1][j + 1].y, outp[i + 1][j + 1].z);
-         glVertex3f(outp[i][j + 1].x, outp[i][j + 1].y, outp[i][j + 1].z);
-         glEnd();
-      }
-   }
+void desenharSuperficie(float setY, GLfloat* corAmb, GLfloat* corDif) {
+    glMaterialfv(GL_FRONT, GL_SPECULAR, branco);
+    glMaterialfv(GL_FRONT, GL_AMBIENT, corAmb);
+    glMaterialfv(GL_FRONT, GL_DIFFUSE, corDif);
+    glMaterialf(GL_FRONT, GL_SHININESS, 120);
+
+    for (int i = 0; i < RESOLUTIONI - 1; i++) {
+        for (int j = 0; j < RESOLUTIONJ - 1; j++) {
+            XYZ A = outp[i][j];
+            XYZ B = outp[i+1][j];
+            XYZ C = outp[i+1][j+1];
+            XYZ D = outp[i][j+1];
+
+            A.y += setY;
+            B.y += setY;
+            C.y += setY;
+            D.y += setY;
+
+            XYZ u = { B.x - A.x, B.y - A.y, B.z - A.z };
+            XYZ v = { D.x - A.x, D.y - A.y, D.z - A.z };
+
+            XYZ normal = {
+                u.y * v.z - u.z * v.y,
+                u.z * v.x - u.x * v.z,
+                u.x * v.y - u.y * v.x
+            };
+
+            float len = sqrt(normal.x * normal.x + normal.y * normal.y + normal.z * normal.z);
+            if (len != 0.0f) {
+                normal.x /= len;
+                normal.y /= len;
+                normal.z /= len;
+            }
+
+            glBegin(GL_QUADS);
+                glNormal3f(normal.x, normal.y, normal.z);
+                glVertex3f(A.x, A.y, A.z);
+                glVertex3f(B.x, B.y, B.z);
+                glVertex3f(C.x, C.y, C.z);
+                glVertex3f(D.x, D.y, D.z);
+            glEnd();
+        }
+    }
 }
 
-void display(){
+void display() {
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
 
-    //gluLookAt(20, 10, 40,   // câmera mais afastada
-    //          20,  3,  0,   // ponto de interesse central
-    //           0,  1,  0);  // vetor "up"
+    gluLookAt(30, -50, 30, 10, 0, 0, 0, 0, 1);
 
-   gluLookAt(30.0, 50.0, 50.0,
-        0.0, 0.0, 0.0,    
-        0.0, 1.0, 0.0);   
-
-    int nRows = 3;
-    int nCols = 1;
-    float spacingX = 10.0;
-    float spacingY = 10.0;
-
-    for (int i = 0; i < nCols; i++) {
-        for (int j = 0; j < nRows; j++) {
-            glPushMatrix();
-            glTranslatef(i * spacingX, 0, j * spacingY);
-            generateControlPoint();
-            Surface();
-            drawSurface();
-            glPopMatrix();
-        }
-    }
+    generateControlPoint();
+    Surface();
+    
+    desenharSuperficie(-15, azul_amb, azul_dif);
+    desenharSuperficie(0, verde_amb, verde_dif);
+    desenharSuperficie(15, vermelho_amb, vermelho_dif);
 
     glutSwapBuffers();
 }
 
+void reshape(GLint w, GLint h) {
+    glViewport(0, 0, w, h);
 
-/*
-void display(){
-
-   glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-   
-    GLfloat green[] = { 0.0, 1.0, 0.0, 1.0 };
-    GLfloat red[] = { 1.0, 0.0, 0.0, 1.0 };
-    GLfloat brown[] = { 0.6, 0.3, 0.1, 1.0 };
-    GLfloat white[] = { 1.0, 1.0, 1.0, 1.0 };
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    gluPerspective(45, (float)w / h, 1, 100);
 
     glMatrixMode(GL_MODELVIEW);
-    glLoadIdentity();  // Reset matrix before gluLookAt
-
-    gluLookAt(30.0, 70.0, 30.0,
-        0.0, 0.0, 0.0,    
-        0.0, 1.0, 0.0);   
-
-   generateControlPoint();
-   Surface();
-   drawSurface();
-
-   glFlush();
-   glutSwapBuffers();
-
-}*/
-
-void doFrame(int v) {
-    frameNum++;
-    glutPostRedisplay();
-    glutTimerFunc(100,doFrame,0);
+    glLoadIdentity();
 }
 
-// Função de redimensionamento da janela (Reshape Callback)
-void reshape(int w, int h)
-{
-  glMatrixMode(GL_PROJECTION);
-  GLfloat aspect = GLfloat(w) / GLfloat(h);
-  glLoadIdentity();
-
-/* if (w <= h) {
-    // width is smaller, so stretch out the height
-    glOrtho(-2.5, 2.5, -2.5/aspect, 2.5/aspect, -10.0, 10.0);
-  } else {
-    // height is smaller, so stretch out the width
-    glOrtho(-2.5*aspect, 2.5*aspect, -2.5, 2.5, -10.0, 10.0);
-  }
-  */
-  gluPerspective (60,aspect, 0.1, 100);
-
+void timer(int value) {
+   frame_num+= 1;
+   glutPostRedisplay();
+   glutTimerFunc(70, timer, 0);
 }
 
-void init(){
-    glEnable(GL_BLEND);
-    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-    glEnable(GL_NORMALIZE);
-
+void init() {
+    glEnable(GL_DEPTH_TEST);
     glEnable(GL_LIGHTING);
     glEnable(GL_LIGHT0);
-    glEnable(GL_LIGHT1);
-   GLfloat blue_light[] = { 0.0, 0.0, 1.0, 1.0 };
-   GLfloat light_position[] = { 10.0, 10.0, 10.0, 1.0 };
-
-   glLightfv(GL_LIGHT2, GL_DIFFUSE, blue_light);
-   glLightfv(GL_LIGHT2, GL_POSITION, light_position);
-   glEnable(GL_LIGHT2);
-    glEnable(GL_DEPTH_TEST);    
-
-    glClearColor(0.0, 0.0, 0.0, 0.0); 
+    glEnable(GL_NORMALIZE);
+    glShadeModel(GL_SMOOTH);
 }
 
+int main(int argc, char** argv) {
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
+    glutInitWindowSize(800, 600);
+    glutCreateWindow("Tarefa 6");
 
-/*void init(){
+    init();
+    glutDisplayFunc(display);
+    glutReshapeFunc(reshape);
 
-    glEnable(GL_BLEND);
-    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-    glEnable(GL_NORMALIZE);
+    glutTimerFunc(0, timer, 0);
 
-    glEnable(GL_LIGHTING);                // so the renderer considers light
-    glEnable(GL_LIGHT0);                  // turn LIGHT0 on
-    glEnable(GL_LIGHT1);
-    //glEnable(GL_LIGHT2);  
-    //glEnable(GL_LIGHT3);  
-    //glEnable(GL_LIGHT4);  
-    glEnable(GL_DEPTH_TEST);    
-}*/
-
-// Função Principal
-int main(int argc, char **argv)
-{
-  glutInit(&argc, argv);
-  glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB | GLUT_DEPTH);
-  glutInitWindowPosition(80, 80);
-  glutInitWindowSize(800, 600);
-  glutCreateWindow("Mesa Posta de Jantar");
-  glutReshapeFunc(reshape);
-  glutDisplayFunc(display);
-  glutTimerFunc(200,doFrame,0); 
-  init();
-  glutMainLoop();
+    glutMainLoop();
+    return 0;
 }
